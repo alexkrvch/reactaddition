023c7e61c47e95df8314aca4104fefc4e4690744
Alex <heikun@yandex.ru>
2023-11-14 12:32:45 +0300

    Install react and test

---
+++ src/components/Counter.tsx
@@ -0,0 +1,24 @@
+import React, {useState} from 'react';
+import './Counter.scss';
+
+export const Counter = () => {
+    const [count, setCount] = useState(0)
+
+    const increment = () => {
+        setCount(count + 1)
+    }
+
+    const decrement = () => {
+        setCount(count - 1)
+    }
+
+    return (
+        <div>
+            <div>{count}</div>
+            <button onClick={increment}>Increment
+            </button>
+            <button onClick={decrement}>Decrement
+            </button>
+        </div>
+    );
+};
--- src/test.ts
+++ src/test.ts
@@ -1,4 +1,4 @@
 export const someFN = (arg: number):string => {
-    console.log('test2 42141241222222 24241111 he  ')
+    console.log('t11111est2 42141241222222 24241111 he  jkjlkjlkj')
     return 'Hello'
 };
