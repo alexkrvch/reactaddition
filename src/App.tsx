16444758a22a80d9bf5f8cbf0d0ebe8d12b0bce5
Alex <heikun@yandex.ru>
2023-11-14 14:32:17 +0300

    Add files with const themes and create hook useTheme

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import {Suspense} from 'react';
 import {Counter} from "./components/Counter";
-import './index.scss';
+import './styles/index.scss';
 import {Link, Route, Routes} from 'react-router-dom';
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
+
 
 const App = () => {
+    const {theme, toggleTheme} = useTheme()
+
     return (
-        <div className='app'>
+        <div className={`app ${theme}`}>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
             <Suspense fallback={<div>Loading...</div>}>
@@ -17,6 +21,8 @@ const App = () => {
                 </Routes>
             </Suspense>
             <Counter/>
+
+            <button onClick={toggleTheme}>Change color</button>
         </div>
     );
 };
