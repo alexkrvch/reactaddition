f8a6bbcc8c8f7e7e644c678f74206cb4fd090a03
Alex <heikun@yandex.ru>
2023-11-15 10:14:24 +0300

    Create navbar and use theme in applink

---
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -0,0 +1,26 @@
+import {FC} from 'react';
+import {classNames} from "shared/lib/classNames/classNames";
+import cls from './NavBar.module.scss';
+import {AppLink, AppLinkTheme} from "shared/ui/AppLink/AppLink";
+
+interface NavBarProps {
+    className?: string;
+}
+
+export const NavBar:FC<NavBarProps> = ({className}) => {
+    return (
+        <div className={classNames(cls.navBar, {}, [className])}>
+            <div className={cls.logo}>
+                WebArcticFox
+            </div>
+            <div className={cls.menu}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.menuItem}>
+                    Главная
+                </AppLink>
+                <AppLink theme={AppLinkTheme.RED} to={'/about'} className={cls.menuItem}>
+                    О сайте
+                </AppLink>
+            </div>
+        </div>
+    );
+};
