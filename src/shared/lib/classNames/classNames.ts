f2b187853f699d46dffe96da09a44b3b967824e8
Alex <heikun@yandex.ru>
2023-11-16 16:06:45 +0300

    Added jest, config and first three tests

--- src/shared/lib/classNames/classNames.ts
+++ src/shared/lib/classNames/classNames.ts
@@ -4,9 +4,9 @@ export function classNames (cls: string, mods: Mods = {}, additional: string[] =
     return [
         cls,
         ...additional.filter(Boolean),
-        Object.entries(mods)
+        ...Object.entries(mods)
             .filter(([_, value]) => Boolean(value))
-            .map(([cls, value]) => cls)
+            .map(([cls]) => cls)
     ]
         .join(' ')
 }
