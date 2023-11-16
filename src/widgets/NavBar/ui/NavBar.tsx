106d16c033f4cf7447d6be4af0d5b547b93825cc
Alex <heikun@yandex.ru>
2023-11-16 16:51:41 +0300

    Added pageloader

--- src/widgets/NavBar/ui/NavBar.tsx
+++ src/widgets/NavBar/ui/NavBar.tsx
@@ -2,23 +2,26 @@ import { type FC } from 'react'
 import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './NavBar.module.scss'
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink'
+import { useTranslation } from 'react-i18next'
 
 interface NavBarProps {
     className?: string
 }
 
 export const NavBar: FC<NavBarProps> = ({ className }) => {
+    const { t } = useTranslation()
+
     return (
         <div className={classNames(cls.navBar, {}, [className])}>
             <div className={cls.logo}>
-                WebArcticFox
+                {t('WebArcticFox')}
             </div>
             <div className={cls.menu}>
                 <AppLink theme={AppLinkTheme.SECONDARY} to={'/'} className={cls.menuItem}>
-                    Главная
+                    {t('Главная')}
                 </AppLink>
                 <AppLink theme={AppLinkTheme.RED} to={'/about'} className={cls.menuItem}>
-                    О сайте
+                    {t('О сайте | меню')}
                 </AppLink>
             </div>
         </div>
