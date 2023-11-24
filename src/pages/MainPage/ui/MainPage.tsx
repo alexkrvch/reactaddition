c5ecaa47cda9742e1852ef527c05c62c990fe42d
Alex <heikun@yandex.ru>
2023-11-24 20:24:30 +0300

    Remove input from mainpage

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,21 +1,14 @@
 import { useTranslation } from 'react-i18next'
-import { useState, type ReactElement } from 'react'
+import { type ReactElement } from 'react'
 import { BugButton } from 'app/providers/ErrorBoundary'
-import { Input } from 'shared/ui/Input/Input'
 
 const MainPage = (): ReactElement => {
     const { t } = useTranslation('main')
-    const [value, setValue] = useState('')
-
-    const onChange = (val: string): void => {
-        setValue(val)
-    }
 
     return (
         <div>
             {t('Главная страница')}
             <BugButton />
-            <Input value={value} onChange={onChange} placeholder={'Введи логин'} />
         </div>
     )
 }
