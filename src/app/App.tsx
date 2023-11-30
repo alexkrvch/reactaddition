ced4a71bcec2b721d2afb3d0c0ddfd24f152a759
Alex <heikun@yandex.ru>
2023-11-30 12:39:40 +0300

    update application auth route protection

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,12 +5,13 @@ import { NavBar } from 'widgets/NavBar'
 import { Sidebar } from 'widgets/Sidebar'
 import { type ReactNode, Suspense, useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
-import { userActions } from 'ourEntities/User'
+import { useDispatch, useSelector } from 'react-redux'
+import { getUserInit, userActions } from 'ourEntities/User'
 
 const App = (): ReactNode => {
     const { t } = useTranslation()
     const dispatch = useDispatch()
+    const init = useSelector(getUserInit)
 
     useEffect(() => {
         dispatch(userActions.initAuthData())
@@ -22,7 +23,7 @@ const App = (): ReactNode => {
                 <NavBar/>
                 <div className="content-page">
                     <Sidebar/>
-                    <AppRouter/>
+                    {init && <AppRouter/>}
                 </div>
             </Suspense>
         </div>
