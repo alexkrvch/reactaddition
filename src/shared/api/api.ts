7d51b9381460c6a0b577a3c0b0f7dfb8775925b1
Alex <heikun@yandex.ru>
2023-12-10 15:33:07 +0300

    created file for netlify

--- src/shared/api/api.ts
+++ src/shared/api/api.ts
@@ -2,8 +2,12 @@ import axios from 'axios'
 import { USER_LOCALSTORAGE_KEY } from 'shared/const/localStorage'
 
 export const apiInstance = axios.create({
-    baseURL: _API_,
-    headers: {
-        authorization: localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
+    baseURL: _API_
+})
+
+apiInstance.interceptors.request.use((config) => {
+    if (config.headers) {
+        config.headers.Authorization = localStorage.getItem(USER_LOCALSTORAGE_KEY) || ''
     }
+    return config
 })
