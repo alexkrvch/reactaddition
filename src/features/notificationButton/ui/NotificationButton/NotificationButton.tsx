c05724a3d566cbfb728115ed7dfbdc36dbbce2af
Alex <heikun@yandex.ru>
2024-01-18 13:27:02 +0300

    Added async loading for drag modules and use it in Drawer

--- src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
+++ src/features/notificationButton/ui/NotificationButton/NotificationButton.tsx
@@ -8,6 +8,7 @@ import { Popover } from 'shared/ui/Popups'
 import NotificationIcon from 'shared/assets/icons/notification-20-20.svg'
 import { Drawer } from 'shared/ui/Drawer/Drawer'
 import { BrowserView, MobileView } from 'react-device-detect'
+import { AnimationProvider } from 'shared/lib/components/AnimationProvider'
 
 interface NotificationButtonProps {
     className?: string
@@ -45,9 +46,11 @@ export const NotificationButton: FC<NotificationButtonProps> = memo((props) => {
             </BrowserView>
             <MobileView>
                 {trigger}
-                <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
-                    <NotificationList/>
-                </Drawer>
+                <AnimationProvider>
+                    <Drawer isOpen={isOpen} onClose={onCloseDrawer}>
+                        <NotificationList/>
+                    </Drawer>
+                </AnimationProvider>
             </MobileView>
         </>
     )
