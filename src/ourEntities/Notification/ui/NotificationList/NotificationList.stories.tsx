ec1c1e0166c988e18b818b09ab3eae7798abd32e
Alex <heikun@yandex.ru>
2024-01-22 21:01:21 +0300

    Fixed storybook and restore mock data for stories

--- src/ourEntities/Notification/ui/NotificationList/NotificationList.stories.tsx
+++ src/ourEntities/Notification/ui/NotificationList/NotificationList.stories.tsx
@@ -3,12 +3,25 @@ import { NotificationList } from './NotificationList'
 import { Theme } from '@/app/providers/ThemeProvider'
 import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator'
 import React from 'react'
+import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator'
 
 const meta = {
-    title: 'shared/NotificationList',
+    title: 'entities/NotificationList',
     component: NotificationList,
     parameters: {
-        layout: 'fullscreen'
+        layout: 'fullscreen',
+        mockData: [
+            {
+                url: `${_API_}/notifications`,
+                method: 'GET',
+                status: 200,
+                response: [
+                    { id: '1', title: 'Уведомление 1', description: 'Описание уведомления' },
+                    { id: '2', title: 'Уведомление 2', description: 'Описание уведомления' },
+                    { id: '3', title: 'Уведомление 3', description: 'Описание уведомления' }
+                ]
+            }
+        ]
     },
     tags: ['autodocs']
 } satisfies Meta<typeof NotificationList>
@@ -17,16 +30,38 @@ export default meta
 type Story = StoryObj<typeof meta>
 
 export const PrimaryLight: Story = {
-    args: {}
+    args: {},
+    decorators: [
+        (Story) => (
+            <StoreDecorator state={
+                {
+                    user: {
+                        authData: { id: '1', username: 'admin' }
+                    }
+                }}>
+                <ThemeDecorator theme={Theme.LIGHT}>
+                    <Story/>
+                </ThemeDecorator>
+            </StoreDecorator>
+        )
+    ]
 }
 
 export const PrimaryDark: Story = {
     args: {},
     decorators: [
         (Story) => (
-            <ThemeDecorator theme={Theme.DARK}>
-                <Story/>
-            </ThemeDecorator>
+            <StoreDecorator state={
+                {
+                    user: {
+                        authData: { id: '1', username: 'admin' }
+                    }
+                }}>
+
+                <ThemeDecorator theme={Theme.DARK}>
+                    <Story/>
+                </ThemeDecorator>
+            </StoreDecorator>
         )
     ]
 }
@@ -35,9 +70,16 @@ export const PrimaryOrange: Story = {
     args: {},
     decorators: [
         (Story) => (
-            <ThemeDecorator theme={Theme.ORANGE}>
-                <Story/>
-            </ThemeDecorator>
+            <StoreDecorator state={
+                {
+                    user: {
+                        authData: { id: '1', username: 'admin' }
+                    }
+                }}>
+                <ThemeDecorator theme={Theme.ORANGE}>
+                    <Story/>
+                </ThemeDecorator>
+            </StoreDecorator>
         )
     ]
 }
